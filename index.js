4192872a71f9fefc83c66ee833661da4fe051579
NoridgeStonFactory <n.ishibashi@norifac.net>
2025-07-29 16:11:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ import express from "express";
 import { lineMiddleware, eventHandle, lineClient } from "./line.js";
 import { redisRouter } from "./redis.js";
 
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// receive line message
 app.post("/callback", lineMiddleware, (req, res) => {
   Promise.all(req.body.events.map(eventHandle))
     .then((result) => res.json(result))
@@ -15,28 +12,27 @@ app.post("/callback", lineMiddleware, (req, res) => {
 
 function errorHandle(error, res) {
   console.error("error", error);
-  switch (error.name) {
-    case "chatGPT":
-      let responseMessage = "GPT異常，請報修工程師。";
-      switch (error.status) {
-        case 429:
-          responseMessage = "GPT暫時無法使用，請稍後再試。";
-          break;
-        default:
-          break;
-      }
-      res.json(lineClient.replyMessage(error.replyToken, { type: "text", text: responseMessage }));
-      break;
-    default:
-      res.status(500).end();
-      break;
+  if (error.name === "chatGPT") {
+    let responseMessage = "GPT異常，請報修工程師。";
+    if (error.status === 429) {
+      responseMessage = "GPT暫時無法使用，請稍後再試。";
+    }
+    res.json(
+      lineClient.replyMessage(error.replyToken, {
+        type: "text",
+        text: responseMessage,
+      })
+    );
+  } else {
+    res.status(500).end();
   }
 }
 
 app.use("/redis", redisRouter);
 
-// listen on port
+// ポート番号は Render が提供する process.env.PORT を使う
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.info(`listening on ${port}`);
+  console.info(`✅ listening on ${port}`);
 });
+
